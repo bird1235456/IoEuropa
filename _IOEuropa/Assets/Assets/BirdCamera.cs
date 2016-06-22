915d5c350effc0359bbdc77b173722f755673cc1
Mark Stumbris <Mark@birdash.com>
2016-06-22 14:29:34 -0500

    Added file structure and assets

---
+++ _IOEuropa/Assets/Assets/BirdCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdCamera : MonoBehaviour {
+
+	public GameObject target = null;
+	public bool orbitY = false;
+	
+	void Start () {
+	
+	}
+	
+
+	void Update ()
+	{
+		if (target != null)
+		{
+			transform.LookAt(target.transform);
+
+			if (orbitY)
+			{
+				transform.RotateAround(target.transform.position, Vector3.right, Time.deltaTime * 15);
+			}
+		}
+
+
+	}
+}
