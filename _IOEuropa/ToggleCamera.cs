35efb646d0f69fb34ee861640354016ad0979a55
Vineeth15 <nexusdragonartncraft@gmail.com>
2016-06-26 15:31:07 -0500

    IoEuropa Mission 1 Stuff
    IoEuropa Mission 1 Stuff

---
+++ _IOEuropa/ToggleCamera.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleCamera : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] cameras = new GameObject[5];
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("1") && cameras[0] != null)
+        {
+            switchCameras(0);
+        }
+        else if (Input.GetKeyDown("2") && cameras[1] != null)
+        {
+            switchCameras(1);
+        }
+        else if (Input.GetKeyDown("3") && cameras[2] != null)
+        {
+            switchCameras(2);
+        }
+        else if (Input.GetKeyDown("4") && cameras[3] != null)
+        {
+            switchCameras(3);
+        }
+        else if (Input.GetKeyDown("5") && cameras[4] != null)
+        {
+            switchCameras(4);
+        }
+    }
+
+    private void switchCameras(int keyNum)
+    {
+        for (int i = 0; i < cameras.Length - 1; i++)
+        {
+            if (cameras[i] != null && keyNum != i)
+            {
+                // turn camera off
+                cameras[i].GetComponent<Camera>().enabled = false;
+            }
+            else
+            {
+                // turn camera on
+                cameras[i].GetComponent<Camera>().enabled = true;
+            }
+        }
+    }
+}
